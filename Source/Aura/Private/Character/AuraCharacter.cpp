f76cdc28fe24d3b9473783e61d967e8aacbcc95e
pwjfrye <jfrye@pipeworks.com>
2024-06-21 17:28:00 -0700

    Character now rotates towards direction of movement

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	auto* MyCharacterMovement = GetCharacterMovement();
+	MyCharacterMovement->bOrientRotationToMovement = true;
+	MyCharacterMovement->RotationRate = FRotator(0.f, 400.f, 0.f);
+	MyCharacterMovement->bConstrainToPlane = true;
+	MyCharacterMovement->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
